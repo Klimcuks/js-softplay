ea2cb0bc5bb791a0a64d4abf12fe4d9c60b0a619
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2022-07-19 21:30:10 +0100

    assignment completed

--- spec/soft-play.spec.js
+++ spec/soft-play.spec.js
@@ -1,89 +1,88 @@
-const {enter, leave, occupancy, reset} = require('../src/soft-play.js')
+const { enter, leave, occupancy, reset } = require('../src/soft-play.js')
 
-describe("Soft Play", () => {  
-  
-  it("Initial state is empty", function() {
+describe('Soft Play', () => {
+  it('Initial state is empty', function () {
     reset()
-    expect(occupancy()).toEqual({adults: 0, children: 0})
+    expect(occupancy()).toEqual({ adults: 0, children: 0 })
   })
 
-  it("Single adult and child enter", function() {
+  it('Single adult and child enter', function () {
     reset()
-    expect(enter(1,1)).toBeTrue()
-    expect(occupancy()).toEqual({adults: 1, children: 1})
-  })
-  
-  it("Single adult with 2 children cannot enter", function() {
-    reset()
-    expect(enter(1,2)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 0, children: 0})
+    expect(enter(1, 1)).toBeTrue()
+    expect(occupancy()).toEqual({ adults: 1, children: 1 })
   })
 
-  it("2 adults with 1 child can enter", function() {
+  it('Single adult with 2 children cannot enter', function () {
     reset()
-    expect(enter(2,1)).toBeTrue()
-    expect(occupancy()).toEqual({adults: 2, children: 1})
+    expect(enter(1, 2)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 0, children: 0 })
   })
 
-  it("Adult can leave when adults 2 and children 1", function() {
+  it('2 adults with 1 child can enter', function () {
     reset()
-    enter(2,1)
-    expect(leave(1,0)).toBeTrue()
-    expect(occupancy()).toEqual({adults: 1, children: 1})
+    expect(enter(2, 1)).toBeTrue()
+    expect(occupancy()).toEqual({ adults: 2, children: 1 })
   })
 
-  it("Adult cannot leave when adults 1 and children 1", function() {
+  it('Adult can leave when adults 2 and children 1', function () {
     reset()
-    enter(1,1)
-    expect(leave(1,0)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 1, children: 1})
+    enter(2, 1)
+    expect(leave(1, 0)).toBeTrue()
+    expect(occupancy()).toEqual({ adults: 1, children: 1 })
   })
 
-  it("Adult cannot leave when adults 2 and children 2", function() {
+  it('Adult cannot leave when adults 1 and children 1', function () {
     reset()
-    enter(2,2)
-    expect(leave(1,0)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 2, children: 2})
+    enter(1, 1)
+    expect(leave(1, 0)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 1, children: 1 })
   })
 
-  it("More children cannot leave than are in the soft play center", function() {
+  it('Adult cannot leave when adults 2 and children 2', function () {
     reset()
-    expect(enter(1,1)).toBeTrue()
-    expect(leave(1,2)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 1, children: 1})
+    enter(2, 2)
+    expect(leave(1, 0)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 2, children: 2 })
   })
 
-  it("More adults cannot leave than are in the soft play center", function() {
+  it('More children cannot leave than are in the soft play center', function () {
     reset()
-    expect(enter(1,1)).toBeTrue()
-    expect(leave(2,1)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 1, children: 1})
+    expect(enter(1, 1)).toBeTrue()
+    expect(leave(1, 2)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 1, children: 1 })
   })
 
-  it("Child cannot enter on own", function() {
+  it('More adults cannot leave than are in the soft play center', function () {
     reset()
-    expect(enter(0,1)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 0, children: 0})
+    expect(enter(1, 1)).toBeTrue()
+    expect(leave(2, 1)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 1, children: 1 })
   })
 
-  it("Child cannot leave on own", function() {
+  it('Child cannot enter on own', function () {
     reset()
-    enter(1,1)
-    expect(leave(0,1)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 1, children: 1})
+    expect(enter(0, 1)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 0, children: 0 })
   })
 
-  it("Adult cannot leave with multiple children", function() {
+  it('Child cannot leave on own', function () {
     reset()
-    enter(2,2)
-    expect(leave(1,2)).toBeFalse()
-    expect(occupancy()).toEqual({adults: 2, children:2})
+    enter(1, 1)
+    expect(leave(0, 1)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 1, children: 1 })
   })
 
-  it("Multiple adults can leave with multiple children", function() {
+  it('Adult cannot leave with multiple children', function () {
     reset()
-    enter(2,2)
-    expect(leave(2,2)).toBeTrue()
-    expect(occupancy()).toEqual({adults: 0, children:0})
+    enter(2, 2)
+    expect(leave(1, 2)).toBeFalse()
+    expect(occupancy()).toEqual({ adults: 2, children: 2 })
+  })
+
+  it('Multiple adults can leave with multiple children', function () {
+    reset()
+    enter(2, 2)
+    expect(leave(2, 2)).toBeTrue()
+    expect(occupancy()).toEqual({ adults: 0, children: 0 })
   })
 })
--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,6 @@
 // do not change these lines
 
-function reset () {
+function reset() {
   adults = 0
   children = 0
 }
@@ -11,10 +11,50 @@ let children = 0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 
+function enter(numAdults, numChildren) {
+  if (numChildren <= numAdults) {
+    adults += numAdults
+    children += numChildren
+    return true
+  } else {
+    return false
+  }
+}
+
+function leave(numAdults, numChildren) {
+  const remainingAdults = adults - numAdults
+  const remainingChildren = children - numChildren
+
+  
+  if (remainingAdults < 0) {
+    return false
+  }
+
+  if(remainingAdults < remainingChildren) {
+    return false
+  }
+  if (remainingChildren < 0) {
+    return false
+  }
+
+  if (numAdults >= numChildren) {
+    adults -= numAdults
+    children -= numChildren
+    return true
+  } else {
+    return false
+  }
+}
+
+function occupancy() {
+  const occupancyInfo = { adults: adults, children: children }
+  return occupancyInfo
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
   reset: reset
 }
